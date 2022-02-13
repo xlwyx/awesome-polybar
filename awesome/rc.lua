b928fad560ede15f2d46e2e92322de809cfb59ba
xlwyx <83835896+xlwyx@users.noreply.github.com>
2022-02-13 15:36:13 -0500

    Update rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -136,7 +136,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ " ", " ", " ", " ", " ", " " }, s, awful.layout.layouts[1])
+    -- awful.tag({ " ", " ", " ", " ", " ", " " }, s, awful.layout.layouts[1])
 
 end)
 -- }}}
